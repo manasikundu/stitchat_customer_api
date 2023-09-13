ed17aef3dadc21e626e64ccd50b8e0a863d980f5
priya-patra <priya.patra@thoughtspheres.com>
2023-09-13 22:11:33 +0530

    order list

--- controller/orderController.js
+++ controller/orderController.js
@@ -12,40 +12,23 @@ const { generateAccessToken, auth } = require("../jwt");
 // Define the controller function for fetching order details
 exports.orderList = async (req, res) => {
   try {
-    // Extract query parameters from the request
     var {
       id,
       booking_code,
       boutique_id,
       customer_id,
       order_status_id,
-      order_status_name,
     } = req.query;
-    var query = {};
-    if (id) {
-      query.id = id;
-    }
-    if (booking_code) {
-      query.booking_code = booking_code;
-    }
-    if (boutique_id) {
-      query.boutique_id = boutique_id;
-    }
-    if (customer_id) {
-      query.customer_id = customer_id;
-    }
-    if (order_status_id) {
-      query.order_status_id = order_status_id;
-    }
-    if (order_status_name) {
-      query.order_status_name = order_status_name;
-    }
 
-    // Get the method name and API input for tracking
+    var filter = {
+        id,
+        booking_code,
+        boutique_id,
+        customer_id,
+        order_status_id,
+    };
     var method_name = await Service.getCallingMethodName();
     var apiEndpointInput = JSON.stringify(req.body);
-
-    // Track API hit
     var apiTrack = await Service.trackApi(
       req.query.user_id,
       method_name,
@@ -54,25 +37,16 @@ exports.orderList = async (req, res) => {
       req.query.device_info,
       req.ip
     );
-
-    // Fetch a list of orders using your service functions
-    var boutiqueOrders = await orderService.boutiqueOrder();
+    var boutiqueOrders = await orderService.boutiqueOrder(filter)
     var orderList = [];
-
-    // Loop through each order and fetch additional details
     for (var order of boutiqueOrders) {
-      var boutiqueAddress = await orderService.boutiqueAddress();
-      var orderStatusName = await orderService.orderStatus(order.id);
+      var boutiqueAddress = await orderService.boutiqueAddress()
+      var orderStatusName = await orderService.orderStatus(order.id)
       var orderDelivery = await orderService.orderDelivery()
-
-      // Find the order_status_name for the current order's order_status
       var orderStatus = orderStatusName.find(
         (status) => status.id === order.order_status_id
-      );
-
+      )
       var delivery = orderDelivery.find((delivery_date) => delivery_date.order_id === order.id)
-
-      // Create an order object with combined details
       var orderListArray = {
         id: order.id,
         booking_code: order.booking_code,
@@ -82,9 +56,7 @@ exports.orderList = async (req, res) => {
         subtotal_amount: order.subtotal_amount,
         total_payable_amount: order.total_payable_amount,
         order_status: order.order_status_id,
-        order_status_name: orderStatus
-          ? orderStatus.order_status_name
-          : "Unknown",
+        order_status_name: orderStatus.order_status_name,
         delivery_date: delivery.delivery_date,
         boutique_name: boutiqueAddress[0].boutique_name,
         boutique_address: boutiqueAddress[0].address,
@@ -113,14 +85,13 @@ exports.orderList = async (req, res) => {
       });
     }
   } catch (error) {
-    // Handle any errors that may occur during processing
     console.error(error);
     return res.status(500).send({
       result: {
         orderList,
       },
       HasError: true,
-      Message: "An error occurred while processing the request.",
+      Message: "Some error occured.",
     });
   }
 };
--- service/orderService.js
+++ service/orderService.js
@@ -2,30 +2,51 @@ var db = require("../dbConnection");
 
 // order list
 exports.boutiqueOrder = async (filter) => {
-    try {
-        var query = `SELECT
-    id,
-    booking_code,
-    boutique_id,
-    customer_id,
-    total_quantity,
-    subtotal_amount,
-    total_payable_amount,
-    order_status AS order_status_id
-FROM
-    public.sarter__boutique_orders`
-    var result = await db.query(query)
-    console.log("order : ", result)
-    return result[0]  
-    } catch (error) {
-        console.log(error)
-        return error 
-    }  
-}
+  try {
+    var query = `SELECT
+                        id,
+                        booking_code,
+                        boutique_id,
+                        customer_id,
+                        total_quantity,
+                        subtotal_amount,
+                        total_payable_amount,
+                        order_status AS order_status_id
+                        FROM
+                        public.sarter__boutique_orders`;
+    var conditions = [];
+    if (filter) {
+      if (filter.id) {
+        conditions.push(`id = ${filter.id}`);
+      }
+      if (filter.boutique_id) {
+        conditions.push(`boutique_id = ${filter.boutique_id}`);
+      }
+      if (filter.booking_code) {
+        conditions.push(`booking_code = '${filter.booking_code}'`);
+      }
+      if (filter.customer_id) {
+        conditions.push(`customer_id = ${filter.customer_id}`);
+      }
+      if (filter.order_status_id) {
+        conditions.push(`order_status = ${filter.order_status_id}`);
+      }
+
+      if (conditions.length > 0) {
+        query += ` WHERE ${conditions.join(" AND ")}`;
+      }
+    }
+    var result = await db.query(query);
+    return result[0];
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
 exports.boutiqueAddress = async (filter) => {
-    try {
-        var query = `SELECT
+  try {
+    var query = `SELECT
         boutique_name,
         coutry_state,
         city,
@@ -35,46 +56,46 @@ exports.boutiqueAddress = async (filter) => {
     FROM
         public.sarter__boutique_basic_info
         WHERE
-        id IN (SELECT boutique_id FROM public.sarter__boutique_orders)`
-        var result = await db.query(query)
-        return result[0]
-    } catch (error) {
-        console.log(error)
-        return error 
-    }
-}
+        id IN (SELECT boutique_id FROM public.sarter__boutique_orders)`;
+    var result = await db.query(query);
+    return result[0];
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
 exports.orderStatus = async (filter) => {
-    try {
-        var query = `SELECT
+  try {
+    var query = `SELECT
         id,
         status AS order_status_name
     FROM
         public.sarter__order_status_dic
     WHERE
-        id IN (SELECT order_status FROM public.sarter__boutique_orders)`
-        var result = await db.query(query)
-        return result[0]
-    } catch (error) {
-        console.log(error)
-        return error     
-    }
-}
+        id IN (SELECT order_status FROM public.sarter__boutique_orders)`;
+    var result = await db.query(query);
+    return result[0];
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
-exports.orderDelivery = async (filter) => {
-    try {
-        var query = `SELECT
+exports.orderDelivery = async () => {
+  try {
+    var query = `SELECT
         order_id,
         delivery_date
     FROM
         public.sarter__boutique_orders_items
 		WHERE 
-		 order_id IN (SELECT id FROM public.sarter__boutique_orders)`
-        var result = await db.query(query)
-        return result[0]
-    } catch (error) {
-        console.log(error)
-        return error  
-    }
-}
+		 order_id IN (SELECT id FROM public.sarter__boutique_orders)`;
 
+    var result = await db.query(query);
+    return result[0];
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
