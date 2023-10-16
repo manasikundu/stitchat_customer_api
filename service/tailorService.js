b3daac8d028342e86e7d840aeac35313e40c1775
priya-patra <priya.patra@thoughtspheres.com>
2023-10-16 15:22:55 +0530

    item list forr tailor

--- service/tailorService.js
+++ service/tailorService.js
@@ -2,7 +2,7 @@ const db = require("../dbConnection")
 
 exports.getItemForTailor = async () => {
     try {
-        var query = `select * from sarter__category_item_dic where parent_id in (select id from sarter__category_item_dic where parent_id=1)limit 10`
+        var query = `select * from sarter__category_item_dic where parent_id in (select id from sarter__category_item_dic where parent_id=1)limit 15`
         var result = await db.query(query)
         return result[0]
     } catch (error) {
