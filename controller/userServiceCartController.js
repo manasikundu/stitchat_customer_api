f34b5c206bba4569084165944b10d01c7654fa38
manasikundu <manasi.kundu@thoughtspheres.com>
2023-10-18 15:43:04 +0530

    cart list

--- controller/userServiceCartController.js
+++ controller/userServiceCartController.js
@@ -76,7 +76,10 @@ exports.getCart = async (req, res) => {
       if (user) {
         var data = await cartService.getCartByUserId(user_id)
         if (data.length != 0) {
-          return res.status(200).send({ message: "Successfully fetched data", HasError: false, result: data });
+          const sub_total=data.reduce((total,num)=>total + parseFloat(num.amount),0)
+          const delivery_fee=50
+          const total=parseFloat(sub_total)+parseFloat(delivery_fee)
+          return res.status(200).send({ message: "Successfully fetched data", HasError: false, result: {data:data,sub_total:sub_total,delivery_fee:`${delivery_fee}`,total:`${total}` } });
         }
       } else {
         return res.status(200).send({ message: "This user doesn't exist", HasError: false });
