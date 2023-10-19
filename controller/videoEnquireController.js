f9b3f939f74ace22eb4956c6076748a11e59e64b
priya-patra <priya.patra@thoughtspheres.com>
2023-10-19 12:55:27 +0530

    video enquire

--- controller/videoEnquireController.js
+++ controller/videoEnquireController.js
@@ -15,22 +15,27 @@ exports.createVideoInquire = async (req, res) => {
         const service_type = req.body.service_type
         const note = req.body.note
         const date_time = req.body.date_time
-        const data = {name, email, guest_email, item_id, service_type, note, date_time}
-        const newService = await videoInquireService.createVideoInquire(data)
-        var enquiry_id =  "STIVI3526" + newService.id.toString().padStart(3, '0')
-        var dataJson = {}
-        dataJson.id = newService.id
-        dataJson.name = newService.name
-        dataJson.email = newService.email
-        dataJson.item_id = newService.item_id
-        dataJson.service_type = newService.service_type
-        dataJson.note = newService.note
-        dataJson.date_time = moment(newService.date_time).format('YYYY-MM-DD HH:mm:ss')
-        dataJson.enquiry_id = enquiry_id
+        const currentDate = new Date();
+        const inputDate = new Date(date_time);
 
-        return res.status(200).send({ HasError: false, Message: "Video Inquiry data inserted successfully.", result: dataJson });
+        if (inputDate < currentDate) {
+            return res.status(400).send({ message: "Invalid date_time.", HasError: true });
+        } else {
+            const data = { name, email, guest_email, item_id, service_type, note, date_time };
+            const newService = await videoInquireService.createVideoInquire(data);
+            var enquiry_id = "STIVI3526" + newService.id.toString().padStart(3, '0');
+            var dataJson = {}
+            dataJson.id = newService.id ? newService.id : 0
+            dataJson.name = newService.name ? newService.name : ''
+            dataJson.email = newService.email ? newService.email : ''
+            dataJson.item_id = newService.item_id ? newService.item_id : ''
+            dataJson.service_type = newService.service_type ? newService.service_type : ''
+            dataJson.note = newService.note ? newService : ''
+            dataJson.date_time = moment(newService.date_time).format('YYYY-MM-DD HH:mm:ss') ? moment(newService.date_time).format('YYYY-MM-DD HH:mm:ss') : ''
+            dataJson.enquiry_id = enquiry_id ?newService.enquiry_id : ''
 
-    
+            return res.status(200).send({ HasError: false, Message: "Video Inquiry data inserted successfully.", result: dataJson });
+        }
     } catch (error) {
         console.error(error)
         return res.status(500).send({ message: "Some error occurred.", HasError: true, error: error.message });
