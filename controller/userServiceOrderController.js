d906ff0039123037d71233fd75e26f0b228e5c43
priya-patra <priya.patra@thoughtspheres.com>
2023-10-21 11:57:02 +0530

    order place

--- controller/userServiceOrderController.js
+++ controller/userServiceOrderController.js
@@ -15,7 +15,14 @@ exports.createOrder = async (req, res) => {
         const status = 0
         var currentDate = new Date();
         var formattedDate = currentDate.toISOString().slice(0, 19).replace("T", " ")
-        const data = { user_id, name, email, mobile_number, address_id, status, created_at: formattedDate, updated_at: formattedDate }
+        const coupon_id = req.body.copon_id || 0
+        const coupon_code = req.body.copon_code || 0
+        const delivery_price = req.body.delivery_price || 0
+        const sum_amount = req.body.sum_amount || 0
+        const discount_price = req.body.discount_price || 0
+        const extra_charge = req.body.extra_charge || 0
+        const total_price = req.body.total_price || 0
+        const data = { user_id, name, email, mobile_number, address_id, status, created_at: formattedDate, updated_at: formattedDate, coupon_id, coupon_code, delivery_price, sum_amount, discount_price, extra_charge, total_price }
         if (req.body.user_id) {
             const user = await Users.findOne({ where: { id: user_id } })
             if (user) {
@@ -47,67 +54,6 @@ exports.createOrder = async (req, res) => {
         } else {
             return res.status(400).send({ message: "Please enter a userId", HasError: true });
         }
-
-        //     const serviceOrdersData = req.body
-        //     var method_name = await Service.getCallingMethodName()
-        //     var apiEndpointInput = JSON.stringify(req.body)
-        //     var apiTrack = await Service.trackApi(req.query.user_id,method_name,apiEndpointInput,req.query.device_id,req.query.device_info,req.ip)
-        //     if (!serviceOrdersData) {
-        //         return res.status(400).send({ HasError: true, Message: "Invalid parameter." })
-        //     } else {
-        //         const results = []
-        //         var hasError = false
-        //         const processOrder = async (orderData) => {
-        //             const user_id = orderData.user_id
-        //             const name = orderData.name
-        //             const email = orderData.email
-        //             const mobile_number = orderData.mobile_number
-        //             const address_id = orderData.address_id
-        //             // const status = orderData.status
-
-
-        //         if (!user_id || !name || !email || mobile_number || !address_id) {
-        //             results.push({ HasError: true, Message: "Invalid parameter." })
-        //             hasError = true
-        //         } else {
-        //             const addressId = await UsersAddress.findOne({ where: { id: address_id } })
-        //             if (!addressId) {
-        //                 results.push({ HasError: true, Message: "Address id does not exist." })
-        //                 hasError = true;
-        //             } else {
-        //                 const userId = await Users.findOne({ where: { id: user_id } })
-        //                 if (!userId) {
-        //                 results.push({ HasError: true, Message: "User id does not exist." })
-        //                 hasError = true;
-        //             } else {
-        //                 const currentDate = new Date();
-        //                 const formattedDate = currentDate.toISOString().slice(0, 19).replace("T", " ")
-        //                 const data = { user_id, total_amount, address_details, address_id, status: 1, created_at: formattedDate, updated_at: formattedDate }
-        //                 const newService = await cartServiceOrder.createServiceCart(data)
-        //                 var dataJson = {}
-        //                 dataJson.id = newService.id
-        //                 dataJson.user_id = newService.user_id
-        //                 dataJson.total_amount = newService.total_amount
-        //                 dataJson.address_details = newService.address_details
-        //                 dataJson.address_id = newService.address_id
-        //                 dataJson.status = newService.status
-        //                 dataJson.created_at = moment(newService.created_at).format('YYYY-MM-DD HH:mm:ss')
-        //                 results.push(dataJson)
-        //                 hasError = true
-        //             }
-        //         }
-        //     }
-        // }
-        // if (Array.isArray(serviceOrdersData)) {
-        //     const promises = serviceOrdersData.map(processOrder)
-        //     await Promise.all(promises)
-        // } else {
-        //     const singleOrderArray = [serviceOrdersData]
-        //     const promises = singleOrderArray.map(processOrder)
-        //     await Promise.all(promises)
-        // }
-        // return res.status(200).send({ HasError: false, Message: "Service orders inserted successfully.", results })
-        // }
     } catch (error) {
         console.error(error)
         return res.status(500).send({ message: "Some error occurred.", HasError: true, error: error.message })
