1ada5a4b7dffb30dde4251c4d4d8026ddcaa5c02
priya-patra <priya.patra@thoughtspheres.com>
2023-10-26 14:59:08 +0530

    coupon model

---
+++ model/couponModel.js
@@ -0,0 +1,70 @@
+const DataTypes = require("sequelize")
+const sequelize = require("../dbConnection")
+
+var Coupon = sequelize.define(
+  "sarter__master_coupon",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    coupon_code: {
+      type: DataTypes.STRING(100), 
+      allowNull: false,
+    },
+    coupon_name: {
+      type: DataTypes.STRING(255), 
+    },
+    description: {
+      type: DataTypes.TEXT, 
+    },
+    coupon_type: {
+      type: DataTypes.SMALLINT,
+      defaultValue: 0, 
+    },
+    minimum_order_amount: {
+      type: DataTypes.DECIMAL(10, 2), 
+      defaultValue: 0,
+    },
+    discount_amount: {
+      type: DataTypes.DECIMAL(10, 2), 
+      defaultValue: 0,
+    },
+    start_date: {
+      type: DataTypes.DATE, 
+    },
+    end_date: {
+      type: DataTypes.DATE, 
+    },
+    location: {
+      type: DataTypes.TEXT, 
+    },
+    max_users: {
+      type: DataTypes.INTEGER, 
+      defaultValue: 0,
+    },
+    user_count: {
+      type: DataTypes.INTEGER, 
+      defaultValue: 0,
+    },
+    status: {
+      type: DataTypes.SMALLINT, 
+      defaultValue: 1,
+    },
+    created_at: {
+      type: DataTypes.DATE, 
+    },
+    updated_at: {
+      type: DataTypes.DATE, 
+    },
+  },
+  {
+    sequelize,
+    modelName: "sarter__master_coupon",
+    tableName: "sarter__master_coupon", 
+    timestamps: false,
+  }
+)
+
+module.exports = Coupon
