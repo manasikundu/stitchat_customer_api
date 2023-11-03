98f84c37eac348f2b54d0364c45fe5ce2f610fb9
manasikundu <manasi.kundu@thoughtspheres.com>
2023-11-03 12:15:37 +0530

    coupon add

--- controller/couponController.js
+++ controller/couponController.js
@@ -17,16 +17,17 @@ exports.createCoupon = async (req, res) => {
         const start_date = req.body.start_date
         const end_date = req.body.end_date
         const location = req.body.location
-        const max_users = req.body.max_users
-        const user_count = req.body.user_count
+        const valid_user = req.body.valid_user
         const status = req.body.status
+        const max_discount = req.body.status
+
         var method_name = await Service.getCallingMethodName()
         var apiEndpointInput = JSON.stringify(req.body)
         var apiTrack = await Service.trackApi(req.query.user_id, method_name, apiEndpointInput, req.query.device_id, req.query.device_info, req.ip)
         if (!coupon_code || !coupon_name || !coupon_type || !minimum_order_amount || !start_date || !end_date) {
             return res.status(500).send({ HasError: true, message: "Invalid parameter." })
         } else {
-            const data = { coupon_code, coupon_name, description, coupon_type, minimum_order_amount, discount_amount, start_date, end_date, location, max_users, user_count, status }
+            const data = { coupon_code, coupon_name, description, coupon_type, minimum_order_amount, discount_amount, start_date, end_date, location, valid_user, status ,max_discount}
             const newCoupon = await couponService.createCoupon(data)
             var dataJson = {}
             dataJson.id = newCoupon.id ? newCoupon.id : 0
@@ -40,10 +41,10 @@ exports.createCoupon = async (req, res) => {
             dataJson.start_date = moment(newCoupon.start_date).format('YYYY-MM-DD HH:mm:ss') ? moment(newCoupon.start_date).format('YYYY-MM-DD HH:mm:ss') : ''
             dataJson.end_date = moment(newCoupon.end_date).format('YYYY-MM-DD HH:mm:ss') ? moment(newCoupon.end_date).format('YYYY-MM-DD HH:mm:ss') : ''
             dataJson.location = newCoupon.location ? newCoupon.location : ''
-            dataJson.max_users = newCoupon.max_users ? newCoupon.max_users : 0
-            dataJson.user_count = newCoupon.user_count ? newCoupon.user_count : 0
+            dataJson.valid_user = newCoupon.valid_user ? newCoupon.valid_user : 0
             dataJson.status = newCoupon.status ? newCoupon.status : 0
             dataJson.status_name = newCoupon.status === 1 ? "Active" : (newCoupon.status === 2 ? "Inactive" : '')
+            dataJson.max_discount = newCoupon.max_discount ? newCoupon.max_discount : 0
 
             return res.status(200).send({ HasError: false, Message: "Coupon data inserted successfully.", result: dataJson })
         }
