afa4bb577830f31344a4a04b8d40fc6d3064facc
manasikundu <manasi.kundu@thoughtspheres.com>
2023-11-07 16:44:31 +0530

    jwt

--- jwt.js
+++ jwt.js
@@ -3,22 +3,24 @@ const jwt = require("jsonwebtoken");
 const secretKey = 'tensorflow'
 
 var generateAccessToken = (mobile_number, user_id) => {
-    return jwt.sign({ mobile_number: mobile_number, user_id: user_id}, secretKey, {
+    return jwt.sign({ mobile_number: mobile_number, user_id: user_id }, secretKey, {
         expiresIn: "24h",
     });
 };
 var auth = (req) => {
     var token = req.headers?.authorization
-    if (!token) {
-        return res.status(401).send({
-            HasError: true,
-            StatusCode: 401,
-            message: "Token not provided",
-          })
-    } else {
-        const decoded = jwt.verify(token, secretKey)
-        return decoded
-        // return jwt.verify(token, secret_key);
-    }
+    const decoded = jwt.verify(token, secretKey)
+    return decoded
+    // if (!token) {
+    //     return res.status(401).send({
+    //         HasError: true,
+    //         StatusCode: 401,
+    //         message: "Token not provided",
+    //       })
+    // } else {
+    //     const decoded = jwt.verify(token, secretKey)
+    //     return decoded
+    //     // return jwt.verify(token, secret_key);
+    // }
 };
 module.exports = { generateAccessToken, auth };
